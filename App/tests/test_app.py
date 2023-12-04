e6240b7814ef6123a9b28e9da624f5b933e49883
Brian Cheruiyot <47404512+littbrian@users.noreply.github.com>
2023-12-04 11:51:14 -0400

    Update test_app.py

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -16,7 +16,8 @@ from App.controllers import (
     add_user_to_comp,
     get_user_rankings
 )
-
+from App.controllers import *
+from App.models import *
 
 LOGGER = logging.getLogger(__name__)
 
@@ -67,8 +68,8 @@ def test_authenticate():
 class UsersIntegrationTests(unittest.TestCase):
 
     def test_create_user(self):
-        user = create_user("rick", "bobpass")
-        assert user.username == "rick"
+        result = create_user("rick", "bobpass")
+        assert result is True
 
     def test_get_all_users_json(self):
         users_json = get_all_users_json()
@@ -106,4 +107,80 @@ class UsersIntegrationTests(unittest.TestCase):
         
         self.assertListEqual([{"id":1, "comp_id": 1 , "user_id": 1, "rank": 4}], users)
 
+class RankingPlatformTests(unittest.TestCase):
+
+   
+   
+    def test_register_user_for_competition(self):
+        # Assuming test data setup with a user and competition IDs
+        user_id = 1
+        competition_id = 1
+        rank = 10
+
+        # Call the function to register the user for the competition
+        result = register_user_for_competition(user_id, competition_id, rank)
+
+        # Assert if the function returns True for successful registration
+        assert result is True
+
+    
+
+    def test_update_user_competition_rank(self):
+        # Assuming you have a user and a competition available in your test environment
+        user_id = 1
+        comp_id = 1
+        initial_rank = 0
+        new_rank = 5
+
+        # Add the user to the competition with an initial rank
+        add_results(user_id, comp_id, initial_rank)
+
+        # Update the user's rank in the competition
+        update_user_competition_rank(user_id, comp_id, new_rank)
+        
+        # Retrieve the updated rank for verification
+        updated_user_comp = UserCompetition.query.filter_by(user_id=user_id, comp_id=comp_id).first()
+        assert updated_user_comp.rank == new_rank
+
+    from unittest.mock import patch
+
+    def test_manage_top_20_and_notify(self):
+       
+        comp_id = 1
+
+        
+        top_20_users = generate_top_20_users()  # Define a function to generate mock top 20 users
+        with patch('your_module.get_top_20_users_in_competition') as mock_get_top_20_users:
+            mock_get_top_20_users.return_value = top_20_users
+
+            # Mock the notify_top_20_users function to avoid actually sending notifications
+            with patch('your_module.notify_top_20_users') as mock_notify_top_20_users:
+                # Call the function being tested
+                manage_top_20_and_notify(comp_id)
+
+                
+                mock_get_top_20_users.assert_called_once_with(comp_id)
+
+                
+                mock_notify_top_20_users.assert_called_once_with(top_20_users)
+    
+    def test_manage_top_20_and_notify(self):
+        comp_id = 1
+
+    # Store notifications instead of sending them
+        notifications = []
+
+    
+    def mock_send_notification(user_id, message):
+        notifications.append((user_id, message))
+    
+   
+    
+        your_module.send_notification = mock_send_notification
+
+    
+        manage_top_20_and_notify(comp_id)
+
+   
+        assert len(notifications) == 20  # Assuming 20 users are notified
 
